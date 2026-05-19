f064b5d3da9fb1e6697a276453648080aad07310
Andrew Chen <andrewdchen@berkeley.edu>
2026-05-19 12:42:24 -0400

    trade correlations

---
+++ scripts/06_Tahoe/04_trade_eval_correlations.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#$ -l h_rt=12:00:00
+#$ -N tahoe_trade_eval_cor
+#$ -m e
+#$ -j y
+#$ -P el-studies
+#$ -pe omp 4
+
+cd /rprojectnb2/montilab-p/projects/brcameta/projects/sig_recon/scripts/06_Tahoe
+module load R/4.4.3
+
+Rscript --verbose 04_trade_eval_correlations.R \
+  --source-cell-line NCI-H23 \
+  --eval-pattern "^(ctrl|10th|90th)_projectcor_(gsva|eigen)\\.rds$"
